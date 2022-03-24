951de6c8b4a616e38b6d76a404e399b84308f063
Patrator <Akindeleagbemuko@gmail.com>
2022-03-24 19:06:39 +0100

    Correction

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,10 @@
-include "main.h"
+#include "main.h"
 
 /**
  * reverse_array - reverses an array of integers
  * @a: array to be reversed
  * @n: number of elements in the array
  */
-
 void reverse_array(int *a, int n)
 {
 	int i, j, tmp;
