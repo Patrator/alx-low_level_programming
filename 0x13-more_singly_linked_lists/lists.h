f65dd72d2836c1bd5e29d9c6c424f3c621977c40
Patrator <Akindeleagbemuko@gmail.com>
2022-04-27 22:18:32 +0100

    3 tasks updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listnode_s - singly linked list
+ * @ptr: points to the list of value
+ * @next: points to the list of node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listnode_s
+{
+	listint_t *ptr;
+	struct listnode_s *next;
+} listnode_t;
+
+/*mandatory task*/
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -35,5 +49,10 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
+
+size_t in_node(const listint_t *head, listnode_t *node);
+listnode_t *add_node(listnode_t **head, const listint_t *node);
+void free_nodes(listnode_t *node);
 
 #endif /*_LISTS_H_*/
