82ac8d9816a882f477846f477c3d796ad5ac200b
Patrator <Akindeleagbemuko@gmail.com>
2022-05-04 17:16:18 +0100

    Task 0 updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,24 +12,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *alc;
 	ssize_t numread, numwrt;
-	
 	fd = open(filename, O_RDONLY);
 	if(fd == -1)
 		return 0;
 	if(filename == NULL)
 		return 0;
-
 	/* malloc space */
 	alc = malloc(sizeof(char)*letters);
 	if(alc==NULL)
 		return(0);
-
 	/* Reading the text file */
 	numread = read(fd, alc,letters);
-
 	/* Write to stdout */
 	numwrt = write(STDOUT_FILENO, alc, numread);
-	
 	free(alc);
 	close(fd);
 	return (numwrt);
