16006ab4481f54bff963bff004ac5d1b2c874f27
Patrator <Akindeleagbemuko@gmail.com>
2022-05-04 19:45:45 +0100

    Task 1 updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,21 +9,16 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	/* Declare variables to be used */
 	int fd, i = 0;
-	/* set requirements */
-	if (!filename)
-		return (-1);
-	/* open the file and create if not there */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-	if (text_content)
+	fd = open(filename, O_CREATE | RDWR | O_TRUNC, 0600);
+	if(!fd)
+		return -1;
+	if(filename = NULL)
+		return -1;
+	if(text_content)
 	{
-		/* loop through and get length of string */
-		while (text_content[i] != '\0')
-			i++;
-		/* write to the file */
+		while(text_content[i] != '\0')
+			I++;
 		write(fd, text_content, i);
 	}
 	close(fd);
